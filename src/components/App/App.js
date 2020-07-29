c22edf4d0adfdea46204c25a167b33eda565a109
taofik-adeniyi <bidemi64@gmail.com>
2020-07-29 16:43:31 +0100

    hydrating the state

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import OptionsPanel from '../OptionsPanel'
 import Board from '../Board'
+import { createTiles } from '../../misc/utils'
 
 import './App.css';
 
 class App extends Component{
-
-  constructor(props) {
+    constructor(props) {
     super(props)
-    setState = {
+
+    this.state = {
       numTiles: 36,
       playing: false,
       previousTileIndex: null,
@@ -17,18 +18,25 @@ class App extends Component{
     }
   }
 
-  render() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        Turbo-Matcher
-      </header>
-        <OptionsPanel />
-        <Board />
-      }
-    </div>
-  );
+  startGame = (numTiles) => {
+    this.setState((state) => ({
+      playing: true,
+      previousTileIndex: null,
+      toBeCleared: null,
+      tiles: createTiles(state.numTiles)
+    }))
+  }
 
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          Turbo-Matcher
+        </header>
+          <OptionsPanel playing={this.state.playing} numTiles={this.state.numTiles} startGame={this.startGame} />
+          <Board numTiles={this.state.numTiles} tiles={this.state.tiles} />
+      </div>
+    );
   }
 }
 
